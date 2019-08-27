1f528d4a1735854e7e7344e726b029ff156e848d
William <w.wischlinsky@me.com>
2019-08-27 23:31:04 +0200

    Rebuild wasm

---
+++ runtime/wasm/target/wasm32-unknown-unknown/release/deps/substrate_test_runtime-038a21fb01d4937f.d
@@ -0,0 +1,8 @@
+/Users/William/dev/rust/substrate-blockchain-test/runtime/wasm/target/wasm32-unknown-unknown/release/deps/substrate_test_runtime-038a21fb01d4937f.rmeta: /Users/William/dev/rust/substrate-blockchain-test/runtime/src/lib.rs /Users/William/dev/rust/substrate-blockchain-test/runtime/src/template.rs
+
+/Users/William/dev/rust/substrate-blockchain-test/runtime/wasm/target/wasm32-unknown-unknown/release/deps/libsubstrate_test_runtime-038a21fb01d4937f.rlib: /Users/William/dev/rust/substrate-blockchain-test/runtime/src/lib.rs /Users/William/dev/rust/substrate-blockchain-test/runtime/src/template.rs
+
+/Users/William/dev/rust/substrate-blockchain-test/runtime/wasm/target/wasm32-unknown-unknown/release/deps/substrate_test_runtime-038a21fb01d4937f.d: /Users/William/dev/rust/substrate-blockchain-test/runtime/src/lib.rs /Users/William/dev/rust/substrate-blockchain-test/runtime/src/template.rs
+
+/Users/William/dev/rust/substrate-blockchain-test/runtime/src/lib.rs:
+/Users/William/dev/rust/substrate-blockchain-test/runtime/src/template.rs:
