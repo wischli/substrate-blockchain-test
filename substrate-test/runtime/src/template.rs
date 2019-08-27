13f41d813f147f086c74d062798cb03394079c9c
William <w.wischlinsky@me.com>
2019-08-27 22:17:42 +0200

    Init value functionality

--- substrate-test/runtime/src/template.rs
+++ substrate-test/runtime/src/template.rs
@@ -19,13 +19,9 @@ pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
-/// This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item. 
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
+        Value get(get_value): u64;
 	}
 }
 
@@ -36,30 +32,50 @@ decl_module! {
 		// this is needed only if you are using events in your module
 		fn deposit_event<T>() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		// set current value
+        pub fn set_value(origin, value: u64) -> Result {
+            let sender = ensure_signed(origin)?;
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			<Something<T>>::put(something);
+            <Value<T>>::put(value);
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
-		}
+			Self::deposit_event(RawEvent::SetValue(sender, value));
+
+            Ok(())
+        }
+
+		// increase current_value
+        pub fn increase_value(origin, value: u64) -> Result {
+            let sender = ensure_signed(origin)?;
+
+	        let current_value = Self::get_value();
+
+            <Value<T>>::put(value + current_value);
+
+			Self::deposit_event(RawEvent::IncreaseValue(sender, value));
+
+            Ok(())
+        }
+
+		// decrease current value
+        pub fn decrease_value(origin, value: u64) -> Result {
+            let sender = ensure_signed(origin)?;
+
+	        let current_value = Self::get_value();
+
+            <Value<T>>::put(current_value - value);
+
+			Self::deposit_event(RawEvent::DecreaseValue(sender, value));
+
+            Ok(())
+        }
 	}
 }
 
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		// Just a dummy event.
-		// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		// To emit this event, we call the deposit funtion, from our runtime funtions
-		SomethingStored(u32, AccountId),
+		DecreaseValue(AccountId, u64),
+		IncreaseValue(AccountId, u64),
+		SetValue(AccountId, u64),
 	}
 );
 
@@ -111,13 +127,29 @@ mod tests {
 	}
 
 	#[test]
-	fn it_works_for_default_value() {
+	fn set_value_works() {
 		with_externalities(&mut new_test_ext(), || {
-			// Just a dummy test for the dummy funtion `do_something`
-			// calling the `do_something` function with a value 42
-			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-			// asserting that the stored value is equal to what we stored
-			assert_eq!(TemplateModule::something(), Some(42));
+			assert_ok!(Value::set_value(1, 42));
+			assert_ok!(Value::set_value(1, 0));
+			assert_eq!(Value::set_value(1, 42), Some(42));
+		});
+	}
+	#[test]
+	fn increase_value_works() {
+		with_externalities(&mut new_test_ext(), || {
+			let inc_value = 1337;
+			Value::set_value(1, 42);
+			assert_ok!(Value::increase_value(1, inc_value));
+			assert_eq!(Value::increase_value(1, inc_value), Some(42 + inc_value));
+		});
+	}
+	#[test]
+	fn decrease_value_works() {
+		with_externalities(&mut new_test_ext(), || {
+			let dec_value = 12;
+			Value::set_value(1, 42);
+			assert_ok!(Value::decrease_value(1, dec_value));
+			assert_eq!(Value::decrease_value(1, dec_value), Some(42 - dec_value));
 		});
 	}
 }
